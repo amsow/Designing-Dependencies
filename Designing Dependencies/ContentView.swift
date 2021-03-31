be34564171f589881d5d3d346d94644053363a5d
ads <amadou.s@makeba.money>
2021-03-31 14:07:58 +0000

    Initial Commit

---
+++ Designing Dependencies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Designing Dependencies
+//
+//  Created by Amadou Diarra SOW on 31/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
