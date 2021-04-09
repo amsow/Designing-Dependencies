a5553d49fb3a3f936e57fe6a104ed3e695a5da1b
ads <amadou.s@makeba.money>
2021-04-09 11:54:02 +0000

    Added New target WeatherFeature to hold all implementation stuff for performance's sake

---
+++ WeatherFeature/WeatherFeature.h
@@ -0,0 +1,18 @@
+//
+//  WeatherFeature.h
+//  WeatherFeature
+//
+//  Created by Amadou Diarra SOW on 09/04/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherFeature.
+FOUNDATION_EXPORT double WeatherFeatureVersionNumber;
+
+//! Project version string for WeatherFeature.
+FOUNDATION_EXPORT const unsigned char WeatherFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherFeature/PublicHeader.h>
+
+
